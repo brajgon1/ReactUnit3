41990a874dc7135a4b9431e4ac06d34795129792
Brandon Gonzalez <brajgon@gmail.com>
2024-05-30 23:27:52 -0600

    fixed recipe cards

--- starter/src/components/RecipeCard.jsx
+++ starter/src/components/RecipeCard.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import CarbonaraPic from '../assets/carbonara.jpg';
+import React from "react";
+import CarbonaraPic from "../assets/carbonara.jpg";
+import StrawberryCheeseCake from "../assets/strawberrycheesecake.webp";
 function RecipeCard() {
   return (
-    <div className='recipe-card'>
-        <img src={CarbonaraPic} alt='RecipeCard' className='recipe-img'/>
-        <div className='recipe-details'>
-            <h2 className='name'>Carbonara</h2>
-            <button className='view-btn'>See More</button>
+    <div className="recipe-container">
+      <div className="recipe-card">
+        <img src={CarbonaraPic} alt="carbonara" className="recipe-img" />
+        <div className="recipe-details">
+          <h2 className="name">Carbonara</h2>
+          <button className="view-btn">See More</button>
         </div>
+      </div>
+
+      <div className="recipe-card">
+        <img
+          src={StrawberryCheeseCake}
+          alt="strawberrycheese"
+          className="recipe-img"
+        />
+        <div className="recipe-details">
+          <h2 className="name">Strawberry Cheesecake</h2>
+          <button className="view-btn">See More</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default RecipeCard;
