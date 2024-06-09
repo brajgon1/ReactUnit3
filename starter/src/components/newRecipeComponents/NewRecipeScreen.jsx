66e65d8a711bd6dbcb54a2e408cc8a4cb387172f
Brandon Gonzalez <brajgon@gmail.com>
2024-06-09 12:26:57 -0600

    addIngredient function created

--- starter/src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ starter/src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import { useState } from "react";
 
 const NewRecipeScreen = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+//   Create an arrow function called addIngredient.
+
+// It should setIngredients to the previous values, as well as add an object that contains both name and quantity.
+const addIngredient = () => {
+  setIngredients([...ingredients, { name, quantity }]);
+  setName("");
+  setQuantity("");
+}
+
   const initialValues = {
     type: "",
     recipeName: "",
@@ -25,17 +39,21 @@ const NewRecipeScreen = () => {
           <Form onSubmit={handleSubmit} className="input-container">
             <div className="name-and-img container">
               <input
-              type="text"
-              id="recipeName"
-              value={values.recipeName}
-              onChange={handleChange}
-              placeholder="Name" />
+                type="text"
+                id="recipeName"
+                value={values.recipeName}
+                onChange={handleChange}
+                name="recipeName"
+                placeholder="Name"
+              />
               <input
-              type="text"
-              id="imageURL"
-              value={values.imageURL}
-              onChange={handleChange}
-              placeholder="Image URL" />
+                type="text"
+                id="imageURL"
+                value={values.imageURL}
+                onChange={handleChange}
+                name="imageURL"
+                placeholder="Image URL"
+              />
             </div>
             <div className="radio-container">
               <input type="radio" id="option1" name="recipeType" />
@@ -46,21 +64,47 @@ const NewRecipeScreen = () => {
               <label for="option3">Drink</label>
             </div>
             <div className="prop-cook-serves-container">
-              <input type="text" id="prepTime" placeholder="Prep" />
-              <input type="text" id="cookTime" placeholder="Cook Time" />
-              <input type="text" id="serves" placeholder="Serves" />
+              <input
+                type="text"
+                id="prepTime"
+                value={values.prepTime}
+                onChange={handleChange}
+                name="prepTime"
+                placeholder="Prep"
+              />
+              <input
+                type="text"
+                id="cookTime"
+                value={values.cookTime}
+                onChange={handleChange}
+                name="cookTime"
+                placeholder="Cook Time"
+              />
+              <input
+                type="text"
+                id="serves"
+                value={values.serves}
+                onChange={handleChange}
+                name="serves"
+                placeholder="Serves"
+              />
             </div>
             <div className="ingredients-container">
               <input type="text" id="ingredient" placeholder="Ingredient" />
               <input type="text" id="quantity" placeholder="Quantity" />
+              <div className="add-btn">
+                <button type="submit">Add Another</button>
+              </div>
             </div>
             <div className="textArea-and-btn">
               <div className="container-for-textArea"></div>
               <textarea
                 id="myTextArea"
-                name="myTextArea"
+                name="instructions"
                 rows={6}
                 cols={50}
+                value={values.instructions}
+                onChange={handleChange}
                 placeholder="What are your instructions?"
               ></textarea>
               <button type="submit">Save</button>
